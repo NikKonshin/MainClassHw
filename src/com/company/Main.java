00d4c864cbd723d928ca4cd9c30e36c514d66e4f
NikKonshin <konbshin1996@gmail.c0m>
2020-05-13 18:33:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            byte a = (byte) 8;
+    }
+}
